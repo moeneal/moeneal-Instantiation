2af63c76022c8b084a712dbb28f77be88037a54f
moeneal <momo.mneal@gmail.com>
2023-02-03 03:35:44 +0000

    finished instantiation coding lab 2/2/23

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,9 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        
+        String myStringObject = new String();
+    
+        return myStringObject;
     }
 }
